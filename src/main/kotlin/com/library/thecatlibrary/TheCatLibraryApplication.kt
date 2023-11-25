8eec741a57018869d8faff34bd94cb8d578ad807
Jannayna Araujo <contatojannayna@gmail.com>
2023-11-25 18:55:55 -0300

    Starting project

---
+++ src/main/kotlin/com/library/thecatlibrary/TheCatLibraryApplication.kt
@@ -0,0 +1,11 @@
+package com.library.thecatlibrary
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TheCatLibraryApplication
+
+fun main(args: Array<String>) {
+	runApplication<TheCatLibraryApplication>(*args)
+}
---
+++ src/test/kotlin/com/library/thecatlibrary/TheCatLibraryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.library.thecatlibrary
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TheCatLibraryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
